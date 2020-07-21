9d9eb4c565da786a1b80ce0727c0ea1713163120
Tony Cardella <cardella@gmail.com>
2020-07-21 18:51:47 -0500

    Roughed-in GetTests

---
+++ WebAPI.UnitTests/GetTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using WebAPI.Controllers;
+using WebAPI.Models;
+using Xunit;
+
+namespace WebAPI.UnitTests
+{
+    public class PutCallbackTests
+    {
+        private readonly ThirdPartyController _sut;
+
+        public PutCallbackTests()
+        {
+            _sut = new ThirdPartyController();
+        }
+
+        [Fact]
+        public async Task given_a_valid_string_should_return_204_status_code()
+        {
+            var response = await _sut.PutCallback(new PutBody("My Status", "My Detail"));
+
+            Assert.Equal(204, response.StatusCode);
+        }
+    }
+}
--- WebAPI/Models/GetBody.cs
+++ WebAPI/Models/GetBody.cs
@@ -2,6 +2,17 @@
 {
     public class GetBody
     {
+        public GetBody()
+        {
+        }
+
+        public GetBody(string status, string detail, string body)
+        {
+            Status = status;
+            Detail = detail;
+            Body = body;
+        }
+
         public string Status { get; set; }
         public string Detail { get; set; }
         public string Body { get; set; }
