a886d998502d4e0b44e8f42d2350ab64d2fba67e
Tony Cardella <cardella@gmail.com>
2020-07-21 22:23:33 -0500

    Added DbContext for CosmosDB

---
+++ WebAPI/Models/Request.cs
@@ -0,0 +1,20 @@
+﻿namespace WebAPI.Models
+{
+    public class Request
+    {
+        public Request()
+        {
+        }
+
+        public Request(string id)
+        {
+            Id = id;
+        }
+
+        public string Id { get; set; }
+        public string PartitionKey { get; set; }
+        public string Status { get; set; }
+        public string Detail { get; set; }
+        public string Body { get; set; }
+    }
+}
---
+++ WebAPI/RequestContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI
+{
+    public class RequestContext : DbContext
+    {
+        public RequestContext(DbContextOptions<RequestContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Request> Requests { get; set; }
+
+        #region Overrides of DbContext
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.HasDefaultContainer("ThirdPartyRequests");
+
+            modelBuilder.Entity<Request>()
+                .ToContainer("ThirdPartyRequests")
+                .HasNoDiscriminator()
+                .HasPartitionKey(r => r.PartitionKey);
+        }
+
+        #endregion
+    }
+}
